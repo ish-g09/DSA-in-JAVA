6f5bf88cf8ab27d19c9a300c74c7541909348c69
Ishika Gupta <ig.valiente09@gmail.com>
2026-09-13 02:01:32 +0530

    Time: 0 ms (100%), Space: 42.3 MB (90.77%) - LeetHub

---
+++ 3516-find-closest-person/3516-find-closest-person.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int findClosest(int x, int y, int z) {
+        if(Math.abs(z-y)>Math.abs(z-x)){
+            return 1;
+        }
+        if(Math.abs(z-y)<Math.abs(z-x)){
+            return 2;
+        }
+        return 0;
+    }
+}
